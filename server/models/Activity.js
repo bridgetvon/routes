3fa5e594dd6006e45cf224e152bc5ec8ddf4010d
brotherson67 <mitchell.brotherson@gmail.com>
2022-02-09 19:25:44 -0700

    issue24- Updated activity model/schema:

--- server/models/Activity.js
+++ server/models/Activity.js
@@ -1,10 +1,17 @@
-const { Schema } = require('mongoose');
+const { Schema, model } = require("mongoose");
+const User = require("./User");
 
-const activitySchema = new Schema(
-    { 
-        activity: { 
-            type: String, 
-            
-        }
-    }
-)
+const activitySchema = new Schema({
+  activity: {
+    type: String,
+  },
+  gearRequired: [gearSchema], // Each activity will have one or more pieces of gear required
+  difficultyLevel: {
+    type: String,
+  },
+  users: [User], // many users will be part of each activity
+});
+
+const Activity = model("Activity", activitySchema);
+
+module.exports = Activity;
