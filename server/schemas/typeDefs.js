4b062995946af9327b68a86413c8b3185a04cdf5
brotherson67 <mitchell.brotherson@gmail.com>
2022-02-09 20:21:17 -0700

    issue21-added Reaction type to typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,3 +1,5 @@
+// TYPEDEFS ARE WHAT YOU'RE LOOKING FOR AND RESOLVERS ARE HOW YOU FIND IT
+
 // import the gql tagged template function
 const { gql } = require("apollo-server-express");
 
@@ -16,6 +18,7 @@ const typeDefs = gql`
     username: String
     thoughts: [Thought]
     friends: [User]
+    friendCount: Int
     activities: [Activity]
     reactions: [Reaction]
     gearItems: [Gear]
@@ -24,12 +27,15 @@ const typeDefs = gql`
   type Activity {
     _id: ID
     activity: String
-    gearRequired: [Gear] 
+    gearRequired: [Gear]
     participants: [User]
   }
 
   type Reaction {
-    
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    user: User
   }
 
   type Query {
