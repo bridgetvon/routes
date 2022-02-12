6178560b69626018b547e3f2a5a9b87b9b14684d
brotherson67 <mitchell.brotherson@gmail.com>
2022-02-12 10:21:13 -0700

    added initial Mutation routes

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,15 +1,25 @@
-const { User, Thought } = require('../models');
+const { User, Thought } = require("../models");
 
-//in resolvers you write the code for what the method is actually doing 
-//query must match typedef definition 
+//in resolvers you write the code for what the method is actually doing
+//query must match typedef definition
 const resolvers = {
-    Query: {
-        // parent as a placeholder parameter
-      thoughts: async (parent, { username }) => {
-        //   ternary operator to check if username exists
-        const params = username ? { username } : {};
-        return Thought.find().sort({ createdAt: -1 });
-    }
-  }
-}
-  module.exports = resolvers;
+  Query: {
+    // parent as a placeholder parameter
+    thoughts: async (parent, { username }) => {
+      //   ternary operator to check if username exists
+      const params = username ? { username } : {};
+      return Thought.find().sort({ createdAt: -1 });
+    },
+  },
+
+  Mutation: {
+    createUser: async () => {},
+    login: async () => {},
+    postThought: async () => {},
+    addReaction: async () => {},
+    addFriend: async () => {},
+    // addActivity: async() => {},
+    // addGear: async() => {},
+  },
+};
+module.exports = resolvers;
