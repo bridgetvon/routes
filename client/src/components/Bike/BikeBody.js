391cb4729218e814e2166223091dcf167bc988ef
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2022-02-14 11:02:47 -0700

    start of addding strava to react

--- client/src/components/Bike/BikeBody.js
+++ client/src/components/Bike/BikeBody.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import {React, useEffect } from 'react';
+import axios from 'axios'
 
 function BikeBody() {
+
+  // interface Node {
+  //   activityPositions: any;
+  //   activityName: string;
+  // }
+
+  // const [nodes, setNodes] = useState<Node[]>([]);
+
+  const clientID = '77814';
+  const clientSecret = process.env.CLIENT_SECRET;
+  const refreshToken = process.env.REFRESH_TOKEN;
+  const grant_type = 'refresh_token';
+  const activities_link = `https://www.strava.com/api/v3/athlete/activities`; 
+  const auth_link = "https://www.strava.com/oauth/token";
+
+  useEffect(() => {
+    async function fetchData() {
+      const stravaAuthResponse = await axios.all([
+        axios.post(`${auth_link}?client_id=${clientID}&client_secret=${clientSecret}&refresh_token=${refreshToken}&grant_type=refresh_token`)
+      ]);
+      
+      const stravaActivityResponse = await axios.get(`${activities_link}?access_token=${stravaAuthResponse[0].data.access_token}`);
+      console.log(stravaActivityResponse.data[0]);
+      // const polylines = [];
+      // for (let i = 0; i < stravaActivityResponse.data.length; i += 1) {
+      //   const activity_polyline = stravaActivityResponse.data[i].map.summary_polyline;
+      //   const activity_name = stravaActivityResponse.data[i].name;
+      //   polylines.push({activityPositions: polyline.decode(activity_polyline), activityName: activity_name});
+      // }
+      // console.log(polylines)
+      // setNodes(polylines);
+    }
+
+    fetchData();
+  }, []);
+
+
   return (
     <div className="container">
-        <div>div 1 lorem ipsum dolor sit amet, consectetur lorem ipsum dolor sit amet</div>
+        <div>testing</div>
         <div>div 2 lorem ipsum dolor sit amet, consectetur lorem ipsum dolor sit amet</div>
 
     </div>
