7ae2a95abcfe7af039e746d9db3eaf9ad1125eff
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2022-02-17 13:45:56 -0700

    render-strava

--- client/src/pages/strava.js
+++ client/src/pages/strava.js
@@ -8,8 +8,9 @@ import "../utils/strava";
 // import polyline from '@mapbox/polyline'
 
 function StravaActivities() {
-  
+
   // const navigate = useNavigate();
+  var dataStrava;
   const client_id = '77814';
   const params = useParams();
   const [loading, setLoading] = useState(true);
@@ -23,14 +24,15 @@ function StravaActivities() {
   // console.log(term)
   //loading
   useEffect(() => {
+    //try using get activities here
     console.log(typeof url);
-    if(code) {
+    if (code) {
       //loading variable with spinner loading is true 
       getAccessToken(code).then(setLoading(false));
     } else {
       console.log('hi');
     }
-  },[code])
+  }, [code])
 
 
 
@@ -50,7 +52,7 @@ function StravaActivities() {
     window.location.href = `https://www.strava.com/oauth/authorize?client_id=${client_id}&redirect_uri=http://localhost:3000/profile&response_type=code&scope=activity:read_all`
   }
   const authLink = "https://www.strava.com/oauth/token?";
-  
+
   // if (code) {
   function getAccessToken(code) {
 
@@ -68,85 +70,89 @@ function StravaActivities() {
         grant_type: 'authorization_code'
       })
 
-      
+
       //get response json it and pass it to activities
     })
-    .then((res) => res.json())
-    .then(res => getActivities(res));
-    
-    
-
-  // add other function call to get user id
-  // convertAuthToAccess();
-  function getActivities(res) {
-    
-    console.log(res);
-  //   // commented out path to get user profile info from an array
-  //   // https://www.strava.com/api/v3/athlete?access_token=7bc114015e81808697585c211d65f269c319a3cc
-    const activitiesLink = `https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}`
-
-    fetch(activitiesLink)
-      // .then((res) => console.log(res.json()))
-      .then((res) => {
-        return res.json();
-      })
-      .then(data => {
-        console.log(data);
-        setActivityData(data);
-        // dataStrava = data;
-        // var htmlContent = "";
-        // for (let i = 0; i > 5; i++) {
-        // document.getElementById('activity').innerHTML = data[0];
-        // document.getElementById('activity-name').textContent = 'Activity Name: ' + data[0].name;
-
-        // document.getElementById('activity-distance').innerHTML = 'Distance: ' + data[0].distance;
-
-        // document.getElementById('averageSpeed').innerHTML = 'Average Speed: ' + data[0].average_speed;
-
-        // document.getElementById('totalTime').innerHTML = 'Total Time: ' + data[0].elapsed_time;
-
-        // document.getElementById('elevationGain').innerHTML = 'Total Elevation Gain: ' + data[0].total_elevation_gain;
-      })
-  }
-
- 
-
-  //refresh token
-  //https://www.strava.com/oauth/token?client_id=77814&client_secret=ba4cf64706994d406df016b09df6d62ee55edaef&refresh_token=e3c3bce7513bb09b9c19bfd2450855830fb0d313&grant_type=refresh_token
-
-  // const authLink = "https://www.strava.com/oauth/token";
-
-  function reAuthorize() {
-    fetch(authLink, {
-      method: 'post',
-      headers: {
-        'Accept': 'application/json, text/plain, */*',
-        'Content-Type': 'application/json'
-
-      },
-      body: JSON.stringify({
-        client_id: '77814',
-        client_secret: 'ba4cf64706994d406df016b09df6d62ee55edaef',
-        refresh_token: 'e3c3bce7513bb09b9c19bfd2450855830fb0d313',
-        grant_type: 'refresh_token'
-      })
-      //get response json it and pass it to activities
-    }).then((res => res.json()))
-      .then(res => getActivities(res))
-
-  }
-
-  reAuthorize();
+      .then((res) => res.json())
+      .then(res => getActivities(res));
   
+
+
+
+    // add other function call to get user id
+    // convertAuthToAccess();
+    function getActivities(res) {
+
+      console.log(res);
+      //   // commented out path to get user profile info from an array
+      //   // https://www.strava.com/api/v3/athlete?access_token=7bc114015e81808697585c211d65f269c319a3cc
+      const activitiesLink = `https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}`
+
+      fetch(activitiesLink)
+        // .then((res) => console.log(res.json()))
+        .then((res) => {
+          return res.json();
+        })
+        .then(data => {
+          console.log(data);
+          dataStrava = data;
+          setActivityData(data);
+          // dataStrava = data;
+          // var htmlContent = "";
+          // for (let i = 0; i > 5; i++) {
+          // document.getElementById('activity').innerHTML = data[0];
+          // document.getElementById('activity-name').textContent = 'Activity Name: ' + data[0].name;
+
+          // document.getElementById('activity-distance').innerHTML = 'Distance: ' + data[0].distance;
+
+          // document.getElementById('averageSpeed').innerHTML = 'Average Speed: ' + data[0].average_speed;
+
+          // document.getElementById('totalTime').innerHTML = 'Total Time: ' + data[0].elapsed_time;
+
+          // document.getElementById('elevationGain').innerHTML = 'Total Elevation Gain: ' + data[0].total_elevation_gain;
+        })
+    }
+
+
+
+    //refresh token
+    //https://www.strava.com/oauth/token?client_id=77814&client_secret=ba4cf64706994d406df016b09df6d62ee55edaef&refresh_token=e3c3bce7513bb09b9c19bfd2450855830fb0d313&grant_type=refresh_token
+
+    // const authLink = "https://www.strava.com/oauth/token";
+
+    function reAuthorize() {
+      fetch(authLink, {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+
+        },
+        body: JSON.stringify({
+          client_id: '77814',
+          client_secret: 'ba4cf64706994d406df016b09df6d62ee55edaef',
+          refresh_token: 'e3c3bce7513bb09b9c19bfd2450855830fb0d313',
+          grant_type: 'refresh_token'
+        })
+        //get response json it and pass it to activities
+      }).then((res => res.json()))
+        .then(res => getActivities(res))
+
+    }
+
+    reAuthorize();
+
   }
+  console.log(dataStrava);
+
   return (
     <div className="app-activities">
       <h1>Strava Activities</h1>
       <button onClick={handleClick}>Strava Login</button>
-    <BikeBody />
+      <BikeBody data={dataStrava} />
     </div>
-      
-    
+
+
   );
 
 
